f4beeeca03590e474fc06028faff027c65b1c4d1
Akshat Kothavade <akshat123436@gmail.com>
2022-12-23 06:36:16 +0530

    improved formatting for displaying contest name and remaining time

--- controllers/home.js
+++ controllers/home.js
@@ -9,12 +9,22 @@ module.exports = {
     await axios
       .get("https://codeforces.com/api/contest.list")
       .then((response) => {
-        res.send(response.data);
+        var upcoming_contest = [];
+
+        for (let i = 0; i <= 10; i++) {
+          if (response.data.result[i].relativeTimeSeconds < 0)
+            upcoming_contest.push([
+              response.data.result[i].name,
+              -response.data.result[i].relativeTimeSeconds,
+            ]);
+        }
+        upcoming_contest.reverse();
+        const title = "home";
+        res.render("home", { upcoming_contest, title });
       })
       .catch((err) => {
         console.log(err);
         res.send("error");
       });
-    // res.send("hii");
   },
 };
