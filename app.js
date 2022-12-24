a00f3d40a97f895d4571fef74ee001c0a677929e
Akshat Kothavade <akshat123436@gmail.com>
2022-12-24 14:57:46 +0530

    some changes in layout and other things, just saving this one, gotta change the whole layout

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 const path = require("path");
 const ejsmate = require("ejs-mate");
 const normalRoutes = require("./routes/normalRoutes");
+const expressError = require("./utils/errorclass");
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
@@ -14,6 +15,20 @@ app.use("/", normalRoutes);
 // app.get("*", (req, res) => {
 //   res.send("No default link encountered");
 // });
+
+app.all("*", (req, res, next) => {
+  console.log("invalid link");
+  next(new expressError("Page not found", 404));
+});
+
+app.use((err, req, res, next) => {
+  if (!err.message) {
+    err.message = "Something Went wrong";
+  }
+  const { status = 500 } = err;
+  const title = "Error";
+  res.status(status).render("error", { err, title });
+});
 
 app.listen(3000, () => {
   console.log("on port 3000");
---
+++ utils/errorclass.js
@@ -0,0 +1,9 @@
+class expressError extends Error {
+  constructor(message, status) {
+    super();
+    this.message = message;
+    this.status = status;
+  }
+}
+
+module.exports = expressError;
