266aac48a415fd1b948ef88df37af2c2a2aea639
Akshat Kothavade <akshat123436@gmail.com>
2022-12-25 10:21:00 +0530

    cleaned show of blogs and added centerhead partial

--- controllers/blog.js
+++ controllers/blog.js
@@ -5,9 +5,10 @@ module.exports = {
   blogShow: async (req, res, next) => {
     // const allblogs = await blogs.find({});
     const allblogs = await Blog.find({ author: "Akshat" });
-    console.log(Blog);
+    // console.log(Blog);
     const title = "blogs";
-    console.log(allblogs);
-    res.render("blogs", { title, allblogs });
+    const heading = "BLOGS";
+    // console.log(allblogs);
+    res.render("blogs", { title, allblogs, heading });
   },
 };
