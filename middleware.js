88333bc6364d80aa446d21287bba6ef87d1c4ade
Akshat Kothavade <akshat123436@gmail.com>
2022-12-27 13:20:39 +0530

    added link in your collections

--- middleware.js
+++ middleware.js
@@ -1,6 +1,5 @@
 module.exports = {
   isLoggedin: (req, res, next) => {
-    console.log("IS logged in called");
     if (!req.user) {
       req.flash("error", "Please login");
       return res.redirect("/login");
